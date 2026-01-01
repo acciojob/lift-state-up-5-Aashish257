aea92c6973d3455d531de27d8340f44bb79ab4e2
Aashish Shilkande <83578134+Aashish257@users.noreply.github.com>
2026-01-01 11:02:14 +0000

    Seconde attempt

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,15 @@ import LoginForm from './LoginForm';
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
 
   return (
-    <div>
-        {/* Do not remove the main div */}
-        <LoginForm isLoggedIn={isLoggedIn} onLogin={handleLogin} />
-    </div>
+    <>
+      {isLoggedIn ? (
+        <h2>You are logged in</h2>
+      ) : (
+        <LoginForm setIsLoggedIn={setIsLoggedIn} />
+      )}
+    </>
   )
 }
 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,39 +1,14 @@
 import React, { useState } from 'react';
 
-const LoginForm = ({ isLoggedIn, onLogin }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const LoginForm = ({ setIsLoggedIn }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // For demo purposes, any submit will log in
-    onLogin();
+    setIsLoggedIn(true);
   };
-
-  if (isLoggedIn) {
-    return <div>You are logged in!</div>;
-  }
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
-        <label>Username:</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
       <button type="submit">Login</button>
     </form>
   );
